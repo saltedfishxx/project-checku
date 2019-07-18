d8c62ced6ba48bb9e2e77d404f9fe0e2c5b1bba8
errawinn <errawinn@gmail.com>
2019-07-19 03:23:27 +0800

    get for main

--- project-checku-python/db.py
+++ project-checku-python/db.py
@@ -26,3 +26,48 @@ def createSqlConn():
                               ';Trusted_Connection=yes')
     
     return sql_conn
+
+
+def getCheques(mainStatus):
+
+    if mainStatus == "getRejectedRec":
+        query = "Select PaymentRecord.PaymentID, accountNo, chequeNo, bankNo, BankBrNo as branchNo, PaymentRecord.policyNo as chequePolicyNo, Amount as chequeAmount, Addressee as addresseeCorrect, ChequeSignature as signatureExists, PaymentRecord.PremiumType as chequePremiumType, chequeName, Contact as chequeContact, CONVERT(varchar, date, 1)  as chequeDate , CONVERT(varchar, PaymentRecord.processedAt, 1) as processedDate, PaymentRecord.reviewedBy, PaymentRecord.RejectedReason as rejectReason from Cheque left join PaymentRecord on Cheque.ChequeId = PaymentRecord.ChequeId where PaymentRecord.Status = 'rejected' and PaymentRecord.ReviewedAt is NOT NULL"
+    elif mainStatus == "getSuccessRec":
+        query = "Select PaymentRecord.PaymentID, PaymentRecord.policyNo, Policy.policyType, PaymentRecord.premiumType, Cheque.amount, PaymentRecord.nric, Cheque.chequeName As customerName, CONVERT(varchar, Cheque.date, 1)  As paymentDate, CONVERT(varchar, PolicyAccount.dueDate, 1) , CONVERT(varchar, PaymentRecord.processedAt, 1)  As processedDate, PaymentRecord.reviewedBy from PaymentRecord left join Cheque on PaymentRecord.ChequeId = Cheque.ChequeId left join PolicyAccount on PaymentRecord.policyNo = PolicyAccount.policyNo left join Policy on PolicyAccount.PolicyTypeID = Policy.PolicyTypeID where PaymentRecord.status = 'successful' and PaymentRecord.ReviewedAt IS NOT NULL"
+    elif mainStatus == "getHoldingRec":
+        query = "select PaymentRecord.PaymentID, chequeNo, bankNo, BankBrNo as branchNo, accountNo, PaymentRecord.policyNo, PaymentRecord.premiumType, ChequeName As customerName, contact, amount, CONVERT(varchar, date, 1) As date, ChequeImgFront as imageFront, ChequeImgBack as imageBack, addressee, ChequeSignature as signatureExists  from Cheque left join PaymentRecord on cheque.ChequeId = PaymentRecord.ChequeId where PaymentRecord.status = 'holdingPending' and PaymentRecord.ReviewedAt IS NOT NULL"
+    elif mainStatus == "getPendingRec":
+        query = "select PaymentRecord.PaymentID, chequeNo, bankNo, BankBrNo as branchNo, accountNo, amount, PaymentRecord.policyNo As possiblePolicyNo, PaymentRecord.premiumType, ChequeName As possibleCustomer, contact,  CONVERT(varchar, SMS.SmsSentDate, 22) As dateSmsSent, sms.SmsCount As smsCycle, sms.verified as smsStatus from Cheque left join PaymentRecord on cheque.ChequeId = PaymentRecord.ChequeId left join SMS on PaymentRecord.PaymentID = SMS.PaymentID where PaymentRecord.status = 'holdingPending' and PaymentRecord.ReviewedAt IS NOT NULL"
+        
+    df = pd.read_sql(query, createSqlConn())
+    returnedJson = {"data":[]}  
+    if mainStatus == "getHoldingRec":
+        for i in df.index:
+
+            chequeDetail={"chequeDetail":{
+            "PaymentID":str(df.loc[i]["PaymentID"]),
+            "policyNo":str(df.loc[i]["policyNo"]),
+            "premiumType":str(df.loc[i]["premiumType"]),
+            "customerName":str(df.loc[i]["customerName"]),
+            "contact":str(df.loc[i]["contact"]),
+            "amount":str(df.loc[i]["amount"]),
+            "date":str(df.loc[i]["date"]), 
+            "chequeNo":str(df.loc[i]["chequeNo"]),
+            "bankNo":str(df.loc[i]["bankNo"]),
+            "accountNo":str(df.loc[i]["accountNo"]),
+            "branchNo":str(df.loc[i]["branchNo"]),
+            "imageFront":str(df.loc[i]["imageFront"]),
+            "imageBack":str(df.loc[i]["imageBack"]),
+            "addressee":str(df.loc[i]["addressee"]),
+            "signatureExists":str(df.loc[i]["signatureExists"]) #lmao
+                }
+            }
+            
+            jsondata = json.dumps(chequeDetail, indent=4)       
+            returnedJson["data"].append(json.loads(getAIResult(jsondata)))
+    else:
+        returnedJson = {"data" : df.to_dict(orient='records')}
+                                    
+    returnedJson = json.dumps(returnedJson)  
+    return returnedJson
+
